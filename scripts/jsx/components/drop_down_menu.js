484f21c20b41eb3503bf227322b4aad18c61003c
Laurence-Sonnenberg <laurencesonnenberg01@gmail.com>
2017-03-15 16:41:26 +0200

    Second commit

--- scripts/jsx/components/drop_down_menu.js
+++ scripts/jsx/components/drop_down_menu.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { Form, ControlLabel, FormControl,
+  FormGroup, SplitButton, MenuItem } from 'react-bootstrap';
 
 class DropDownMenu extends Component {
-    render() {
-        return (
-            <div>
-                What Market are you talking to?
-                <DropdownButton bsSize="xsmall" title={this.props.num} id="dropdown-basic-button-1">
-                    <MenuItem eventKey="1">Action</MenuItem>
-                    <MenuItem eventKey="2">Another action</MenuItem>
-                    <MenuItem eventKey="3" active>Active Item</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey="4">Separated link</MenuItem>
-                </DropdownButton>
-            </div>
-        );
-    }
+
+  render() {
+
+    return (
+      <div className="container">
+        <div className="row">
+          <Form inline>
+            <FormGroup>
+              <ControlLabel>What market are you talking to?</ControlLabel>
+              {'  '}
+              <SplitButton className="drop-down-menu" bsStyle ="default" bsSize="xs" title={this.props.num} id="dropdown-basic-button-1">
+                <MenuItem eventKey="1">Action</MenuItem>
+                <MenuItem eventKey="2">Another action</MenuItem>
+                <MenuItem eventKey="3" active>Active Item</MenuItem>
+              </SplitButton>
+            </FormGroup>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DropDownMenu
--- scripts/jsx/index.js
+++ scripts/jsx/index.js
@@ -4,7 +4,8 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
-import DropDownMenuList from './components/drop_down_menu_list'
+//import DropDownMenuList from './components/drop_down_menu_list'
+import DropDownMenu from './components/drop_down_menu'
 import reducer from './reducers'
 
 const store = createStore(reducer,
@@ -13,7 +14,7 @@ const store = createStore(reducer,
 
 ReactDOM.render(
     <Provider store={store}>
-        <DropDownMenuList />
+        <DropDownMenu num = "1" />
     </Provider>,
     document.getElementById('root')
 )
